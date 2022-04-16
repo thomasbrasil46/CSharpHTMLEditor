26d13caf12829167a38855f2f593583919c2212f
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-04-16 19:12:11 -0300

    Editor

--- Menu.cs
+++ Menu.cs
@@ -65,7 +65,7 @@ namespace EditorHTML
         public static void HandleMenuOption(short option)
         {
             switch(option) {
-                case 1: Console.WriteLine("Editor"); break;
+                case 1: Editor.Show(); break;
                 case 2: Console.WriteLine("View"); break;
                 case 0: {
                     Console.Clear();
