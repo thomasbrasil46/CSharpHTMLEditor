22144db4c6c58b91bb1d72a373bbd9b26224d4fe
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-04-16 20:48:49 -0300

    Replace. Finished

--- Viewer.cs
+++ Viewer.cs
@@ -22,6 +22,29 @@ namespace EditorHTML
         {
             var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
             Console.WriteLine(strong);
+            var words = Text.Split(' ');
+
+            for(var i = 0; i < words.Length; i++)
+            {
+                if(strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            (
+                                (words[i].LastIndexOf(' ') -1) - 
+                                words[i].IndexOf('>')
+                            )
+                        )
+                    );
+                    Console.Write(" ");
+                }else {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
